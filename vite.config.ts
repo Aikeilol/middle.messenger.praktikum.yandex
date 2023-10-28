3b5bd8a31040f96134212214d47e09100d22f2da
Vadim Shabinevich <shabinevich.v@mxm.email>
2023-10-28 19:43:45 +0300

    add chat in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default {
         main: resolve(__dirname, 'index.html'),
         authorization: resolve(__dirname, './src/pages/authorization/index.html'),
         registration: resolve(__dirname, './src/pages/registration/index.html'),
+        chat: resolve(__dirname, './src/pages/chat/index.html'),
         404: resolve(__dirname, './src/pages/404/index.html'),
         500: resolve(__dirname, './src/pages/500/index.html'),
       },
